ee87a1e5c042c5093101a3863eced2f844917225
mcy <mcy_1234@163.com>
2024-06-02 20:51:58 +0800

    还原登录逻辑

--- src/permission.js
+++ src/permission.js
@@ -52,10 +52,10 @@ router.beforeEach((to, from, next) => {
       // 在免登录白名单，直接进入
       next()
     } else {
-      Cookies.set("Admin-Token", new Date().getTime(), { expires: 24 * 60 * 60 * 1000 });
-      window.location.reload();
-      // next(`/login?redirect=${to.fullPath}`) // 否则全部重定向到登录页
-      // NProgress.done()
+      // Cookies.set("Admin-Token", new Date().getTime(), { expires: 24 * 60 * 60 * 1000 });
+      // window.location.reload();
+      next(`/login?redirect=${to.fullPath}`) // 否则全部重定向到登录页
+      NProgress.done()
     }
   }
 })
